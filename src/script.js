9df25275de97f185fd39666f9d3e7ddec5694743
Hussein Ali Al-Shamarti <hussein.a220@icloud.com>
2024-02-21 18:23:45 +0100

    HomePage fixes

--- src/script.js
+++ src/script.js
@@ -16,3 +16,31 @@ document.addEventListener("DOMContentLoaded", function () {
     wrapper.classList.add("active-popup");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var slider = document.querySelector(".services-slider");
+  var nextButton = document.querySelector(".next-arrow");
+  var prevButton = document.querySelector(".prev-arrow");
+  var totalCards = slider.querySelectorAll(".service-card").length;
+  var currentIndex = 0;
+
+  function scrollSlider(index) {
+    var cardWidth = slider.querySelector(".service-card").clientWidth; // Width of a service card
+    var newTransformValue = -(cardWidth * index);
+    slider.style.transform = "translateX(" + newTransformValue + "px)";
+  }
+
+  nextButton.addEventListener("click", function () {
+    if (currentIndex < totalCards - 1) {
+      currentIndex++;
+      scrollSlider(currentIndex);
+    }
+  });
+
+  prevButton.addEventListener("click", function () {
+    if (currentIndex > 0) {
+      currentIndex--;
+      scrollSlider(currentIndex);
+    }
+  });
+});
